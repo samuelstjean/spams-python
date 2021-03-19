15fe88e6dc67c8cca3748fdcb1f3e86e125836be
Samuel St-Jean <3030760+samuelstjean@users.noreply.github.com>
2021-03-19 11:13:45 +0100

    tag 2.6.3

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ unlike the official version which can use any blas implementation.
 The source code for this fork is also available at https://github.com/samuelstjean/spams-python/"""
 
 setup(name='spams-bin',
-      version='2.6.2',
+      version='2.6.3',
       description='Python interface for SPAMS - binary wheel with openblas (mac, linux) and intel mkl (windows)',
       long_description=long_description,
       author='Julien Mairal',
