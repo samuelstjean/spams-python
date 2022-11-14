ebf4a467a9768e7508b34d5f16fb3a29aa67bb13
Samuel <3030760+samuelstjean@users.noreply.github.com>
2022-11-14 12:16:11 +0100

    new build of openblas

--- tools/cibw_before_build_win.sh
+++ tools/cibw_before_build_win.sh
@@ -4,19 +4,19 @@ set -xe
 
 # Install Openblas
 # PYTHONPATH=tools python -c "import openblas_support; openblas_support.make_init('spams')"
-mkdir -p /c/openblas/if_32/64/lib/pkgconfig
+mkdir -p /c/opt/openblas/if_32/64/lib/pkgconfig
 
 # delvewheel is the equivalent of delocate/auditwheel for windows.
 python -m pip install delvewheel
 
 # make the DLL available for tools/wheels/repair_windows.sh. If you change
 # this location you need to alter that script.
-mkdir -p /c/openblas/openblas_dll
+mkdir -p /c/opt/openblas/openblas_dll
 which strip
 
 # 64-bit openBLAS
 curl -L https://github.com/scipy/scipy-ci-artifacts/raw/main/openblas_32_if.zip -o openblas_32_if.zip
 unzip openblas_32_if.zip -d /c
-cp /c/openblas/if_32/64/bin/*.dll /c/openblas/openblas_dll
-cp /c/openblas/if_32/64/bin/include /c/openblas/
-cp /c/openblas/if_32/64/bin/lib /c/openblas/
+cp /c/opt/openblas/if_32/64/bin/*.dll /c/opt/openblas/openblas_dll
+cp /c/opt/openblas/if_32/64/bin/include /c/opt/openblas/
+cp /c/opt/openblas/if_32/64/bin/lib /c/opt/openblas/
