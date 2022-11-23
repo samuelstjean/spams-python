4c518802e5102815a3a3e60f41b8bb4009f1f99b
Samuel <3030760+samuelstjean@users.noreply.github.com>
2022-11-23 17:02:37 +0100

    new build of openblas

--- tools/cibw_before_build_linux.sh
+++ tools/cibw_before_build_linux.sh
@@ -1,11 +1,13 @@
 # https://github.com/scipy/scipy/blob/main/tools/wheels/cibw_before_build_linux.sh
 
-set -xe
+# set -xe
 
-# Install Openblas
-basedir=$(python tools/openblas_support.py)
-cp -r $basedir/lib/* /usr/local/lib
-cp $basedir/include/* /usr/local/include
+# # Install Openblas
+# basedir=$(python tools/openblas_support.py)
+# cp -r $basedir/lib/* /usr/local/lib
+# cp $basedir/include/* /usr/local/include
 
 # conda activate openblas
 # conda install openblas libopenblas
+
+yum install openblas-devel lapack-devel -y
