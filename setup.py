15cf7c1f32d9c0c70b9c887a60fab218ac4064e7
Samuel <3030760+samuelstjean@users.noreply.github.com>
2022-12-01 15:35:21 +0100

    add newer wrapper

--- setup.py
+++ setup.py
@@ -92,11 +92,7 @@ def get_config():
 
 
 incs, libs, libdirs, cc_flags, link_flags = get_config()
-
-if platform.system() == 'Windows':
-    source = ['spams_wrap/spams_wrap-windows.cpp']
-else:
-    source = ['spams_wrap/spams_wrap.cpp']
+source = ['spams_wrap/spams_wrap.cpp']
 
 spams_wrap = Extension(
     'spams_wrap._spams_wrap',
