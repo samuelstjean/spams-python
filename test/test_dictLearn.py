fix precision test issue

test/test_dictLearn.py
@@ -480,11 +480,15 @@ def test_lasso_weighted_pos(myfloat):
     alpha1 = spams.lasso(X, D, lambda1=0.0, pos=True).toarray()
     alpha2 = spams.lassoWeighted(X, D, W=W, lambda1=0.0, pos=True).toarray()
     alpha3 = [nnls(D, X[:, i])[0] for i in range(X.shape[1])]
-    alpha3 = np.asarray(alpha3, dtype=myfloat).T
+    alpha3 = np.asarray(alpha3).T
 
     assert np.all(alpha1 >= 0)
     assert np.all(alpha2 >= 0)
     assert np.all(alpha3 >= 0)
 
     np.testing.assert_allclose(alpha1, alpha2)
-    np.testing.assert_allclose(alpha2, alpha3)
+
+    # only tested on double precision because nnls casts internally to double
+    # while spams runs at the given precision
+    if myfloat is np.float64:
+        np.testing.assert_allclose(alpha2, alpha3)